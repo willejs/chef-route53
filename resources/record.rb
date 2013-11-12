82bc9cb5dc4eaacd849c93fe85d584f691f7d5f8
willejs <williamejsalt@gmail.com>
2013-11-12 11:05:47 +0000

    AWS creds arnt required if your using a IAM role!

--- resources/record.rb
+++ resources/record.rb
@@ -6,7 +6,7 @@ attribute :name,                  :kind_of => String
 attribute :value,                 :kind_of => String
 attribute :type,                  :kind_of => String
 attribute :zone_id,               :kind_of => String
-attribute :aws_access_key_id,     :kind_of => String
-attribute :aws_secret_access_key, :kind_of => String
+attribute :aws_access_key_id,     :kind_of => String, :required => false
+attribute :aws_secret_access_key, :kind_of => String, :required => false
 attribute :ttl,                   :kind_of => String, :default => "300"
 attribute :overwrite,             :kind_of => [ TrueClass, FalseClass ], :default => true
